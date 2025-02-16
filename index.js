6387608f49afe90dad52b0b9581f2d792e20e089
ShakoMargania <95220797+ShakoMargania@users.noreply.github.com>
2025-02-16 11:05:08 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var menu = document.getElementById("logos");
+var menus = document.getElementById("ul");
+
+
+
+
+
+menu.addEventListener("click",function(){
+    menus.classList.toggle("active");
+});
+
